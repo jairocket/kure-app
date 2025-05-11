1ea22c0dcbee542a6f26b8521857f6c981adc934
Jailson Anjos <jailsonanjos@hotmail.com>
2025-05-11 13:33:46 -0300

    feat(): Add empty state and remove unncecessary columns

---
+++ lib/components/empty_state.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class EmptyState extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ClipRect(
+            child: Image.asset(
+              'images/empty_chairs.jpg',
+              alignment: Alignment.topCenter,
+              fit: BoxFit.cover,
+              height: 280,
+            ),
+          ),
+          SizedBox(height: 30),
+          TweenAnimationBuilder(
+              child: Center(
+                child: Text(
+                  "Você ainda não tem consultas agendadas",
+                  style: TextStyle(fontSize: 19, color: Colors.black87),
+                ),
+              ),
+              tween: Tween<double>(begin: 0, end: 1),
+              duration: Duration(milliseconds: 1100),
+              builder: (context, _tweenValue, child) {
+                return Opacity(
+                    opacity: _tweenValue,
+                  child: Padding(
+                      padding: EdgeInsets.only(top: _tweenValue * 25),
+                    child: child,
+                  ),
+                );
+              }
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/services/appointments_service.dart
+++ lib/services/appointments_service.dart
@@ -7,8 +7,6 @@ class AppointmentsService {
   final String _appointmentsTableName = 'appointments';
   final String _doctorIdColumnName = 'doctor_id';
   final String _patientIdColumnName = 'patient_id';
-  final String _patientsNameColumnName = 'name';
-  final String _patientsCPFColumnName = 'cpf';
   final String _appointmentDateColumnName = 'date';
   final String _appointmentTimeColumnName = 'time';
 
@@ -18,8 +16,6 @@ class AppointmentsService {
   Future<void> saveAppointment(
     int doctor_id,
     int patient_id,
-    String name,
-    String cpf,
     String date,
     String time,
   ) async {
@@ -29,8 +25,6 @@ class AppointmentsService {
       await db.insert(_appointmentsTableName, {
         _doctorIdColumnName: doctor_id,
         _patientIdColumnName: patient_id,
-        _patientsNameColumnName: name,
-        _patientsCPFColumnName: cpf,
         _appointmentDateColumnName: date,
         _appointmentTimeColumnName: time,
       }, conflictAlgorithm: ConflictAlgorithm.abort);
@@ -46,18 +40,20 @@ class AppointmentsService {
           '''
             SELECT a.id, a.date, a.time, p.name as patient_name FROM $_appointmentsTableName a
               INNER JOIN patients p ON p.id = a.patient_id 
-              WHERE a.doctor_id = ?;
+              WHERE a.doctor_id = ?
+              ORDER BY a.date, a.time ASC;
           ''',
         [doctorId]
       );
+
       if(appointmentsMap.isEmpty) {
-        return [];
+        return List<Map<String, Object>>.empty(growable: true);
       }
 
       print(appointmentsMap.first["id"]);
       print(appointmentsMap.first["patient_name"]);
       print(appointmentsMap.first["date"]);
-      print(appointmentsMap.first["times"]);
+      print(appointmentsMap.first["time"]);
 
       return appointmentsMap.map(
               (appointment) => {
